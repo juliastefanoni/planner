7a63cfdfb4b00811cfb10c1e7986b5d79c4907c6
julia.mendes.acc <julia.mendes@accurate.com.br>
2024-07-12 19:23:30 -0300

    feat: invite participant

---
+++ src/main/java/com/stefanoni/planner/participant/ParticipantCreateResponse.java
@@ -0,0 +1,6 @@
+package com.stefanoni.planner.participant;
+
+import java.util.UUID;
+
+public record ParticipantCreateResponse(UUID id) {
+}
--- src/main/java/com/stefanoni/planner/trip/TripController.java
+++ src/main/java/com/stefanoni/planner/trip/TripController.java
@@ -1,5 +1,7 @@
 package com.stefanoni.planner.trip;
 
+import com.stefanoni.planner.participant.ParticipantCreateResponse;
+import com.stefanoni.planner.participant.ParticipantRequestPayload;
 import com.stefanoni.planner.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -74,5 +78,23 @@ public class TripController {
 
         return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PostMapping("/{id}/invite")
+    public ResponseEntity<ParticipantCreateResponse> inviteParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload) {
+        Optional<Trip> trip = this.repository.findById(id);
+
+        if(trip.isPresent()) {
+            Trip rawTrip = trip.get();
+
+            ParticipantCreateResponse participantResponse = this.participantService.registerParticipantToEvent(payload.email(), rawTrip);
+
+            if (rawTrip.getIsConfirmed()) this.participantService.triggerConfirmationEmailToParticipant(payload.email());
+
+            return ResponseEntity.ok(participantResponse);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 
 }
